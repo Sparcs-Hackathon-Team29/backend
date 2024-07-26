b3cdc554c66e3356e92b5d15a5e7cfc1fcb8cab4
YeongJae0114 <yjl8628@naver.com>
2024-07-26 10:25:02 +0900

    security 의존성 추가

---
+++ src/main/java/com/team29/ArtifactV2/domain/member/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.team29.ArtifactV2.domain.member.controller;
+
+import com.team29.ArtifactV2.domain.member.dto.SignUpDto;
+import com.team29.ArtifactV2.domain.member.service.signUp.SignUpService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequiredArgsConstructor
+@RestController
+public class MemberController {
+    private final SignUpService signUpService;
+
+    @GetMapping("/home")
+    public String check() {
+        return "Success";
+    }
+    @PostMapping("/join")
+    public String joinProcess(SignUpDto joinDTO) {
+
+        System.out.println(joinDTO.getUsername());
+        signUpService.signUp(joinDTO);
+
+        return "ok";
+    }
+}
--- src/main/java/com/team29/ArtifactV2/domain/member/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package com.team29.ArtifactV2.domain.member.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class  MemberService{
-}
